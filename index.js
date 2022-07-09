ba20439f8b5619f880bc275c81d69140224cb8c5
Anant Tiwari <ananttiwariali@gamil.com>
2022-07-09 17:44:23 +0545

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let list= [{
+    heading:'First',
+    description:'The description of todo1'
+},{
+    heading:'Second',
+    description:'The description of todo2'
+},{
+    heading:'Third',
+    description:'The description of todo3'
+},{
+    heading:'Fourth',
+    description:'The description of todo4'
+},{
+    heading:'Fifth',
+    description:'The description of todo5'
+}]
+
+document.getElementsByTagName('body')
