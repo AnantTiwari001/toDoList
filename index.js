721902e2dde9b74cf6eeeb64e1cd7a1cd3d4aec0
Anant Tiwari <ananttiwariali@gamil.com>
2022-07-09 22:07:24 +0545

    buttons done

--- index.js
+++ index.js
@@ -17,3 +17,29 @@ let list= [{
 }]
 
 document.getElementsByTagName('body')
+
+let editBtns= document.getElementsByClassName('edit');
+for(var i=0; i<editBtns.length; i++){
+    editBtns[i].addEventListener('click', editf)
+}
+let deleteBtns= document.getElementsByClassName('delete');
+for(var i=0; i<deleteBtns.length; i++){
+    deleteBtns[i].addEventListener('click', deletef)
+}
+
+function deletef(){
+    this.parentElement.parentElement.remove();
+}
+
+function editf(){
+    let ans= ask();
+    this.parentElement.parentElement.children[0].innerText=ans.tittle;
+    this.parentElement.parentElement.children[1].innerText=ans.description;
+}
+
+function ask(){
+    let tittle=prompt('Enter the tittle of the task');
+    let description= prompt('Enter the description of the task');
+    let pass ={tittle: tittle, description: description};
+    return pass;
+}
